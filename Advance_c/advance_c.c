694bbd554dd913e584c0ea97e27b613671d33ace
Sivapraveen S <54120518+SIVAPRAVEEN333@users.noreply.github.com>
2021-03-24 13:53:01 +0530

    Create advance_c.c

---
+++ Advance_c/advance_c.c
@@ -0,0 +1,148 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#pragma startup fun             // only work on some complier/hardware
+
+void fun()
+{
+    printf("Function executing before main !! \n");
+}
+
+/* executing Function before main !! */
+
+void funcBeforeMain(void) __attribute__ ((constructor));
+
+void funcBeforeMain()
+{
+    printf("Function executing before main !! \n");
+}
+
+/* executing Function after main !! */
+
+void funcAfterMain(void) __attribute__ ((destructor));
+
+void funcAfterMain()
+{
+    printf("\n***********************************************\n");
+    printf("cleanup code after main !! \n");
+    printf("\n***********************************************\n");
+}
+
+int main()
+{
+    /* without variable in the printf statement it prints the garbage value !! */
+    printf("%d \n");                //gives warning !!
+    
+    /* printf returns, n number chars printed, it can also return 0 incase of early matching failure !! */
+    int print_cnt =0;
+    print_cnt = printf("Sivapraveen S \n");
+    printf("size of the name: %d !! \n", print_cnt);
+    
+    /* to print % use %% to display % in side the statement */
+    printf("He got 93%% in maths !! \n");
+
+    /* free a block of memory previously allocated without using free */
+    char* mem;
+    mem = (char*) malloc(100*sizeof(char));     //allocating 100 memory blocks for 'mem'
+    
+    /* note: while printing the size of assined mem block it will print the int/char pointer siz
+    size eg: mem char/int/float whatever the size of pointer will be 4/8 depends upon the 
+    complier */
+
+    realloc(mem, 0);        // memory will be released, it will return the new location in memory, needs to 
+                            // resuse otherwise it will gives u warning !!
+    /* use of %n in printf */
+    {
+        int temp;
+        printf("Hi %n world !! \n", &temp);
+        printf("Known Size of entered word bofore %% n: %d !! \n", temp);
+    }
+
+    /* 3 swaping methods */
+    /* first method */
+    {
+        int a = 1, b = 11;
+    
+        a = a + b;
+        b = a - b;
+        a = a- b;
+    
+        printf("after swap[1M]> a: %d, b: %d !! \n", a, b);
+    }
+    /* second method */
+    {
+        int a = 1, b = 11;
+        
+        a ^= b;
+        b ^= a;
+        a ^= b;
+        printf("after swap[2M]> a: %d, b: %d !! \n", a, b);
+    }
+    /* third method */
+    {
+        int a = 1, b = 11;
+        int temp;
+        
+        temp = a;
+        a = b;
+        b = temp;
+        
+        
+        printf("after swap[3M]> a: %d, b: %d !! \n", a, b);
+        
+    }
+    
+    /* knowing the size of structures !! */
+    typedef struct 
+    {
+        unsigned char c1:3;
+        unsigned char c2:4;
+        unsigned char c3:1;
+    }data_1_t;
+    
+    typedef struct
+    {
+        unsigned char c1:3;
+        unsigned char :0;
+        unsigned char c2:4;
+        unsigned char c3:1;
+    }data_2_t;
+    
+    typedef struct
+    {
+        unsigned char c1;           //1 bytes
+        int c2;                     //4 bytes
+        unsigned char c3[100];      //100 bytes
+        int c4[100];                //400 bytes    
+    }data_3_t;                      // total 1 + 4 + 100 + 400 = 505, while in struct mem allocation will be different somewhat
+                                    // it will take large block of memory even when not in use. refer: struct memory allocation
+    
+    
+    printf("size of struct(data_1): %lu !! \n", sizeof(data_1_t));
+    printf("size of struct(data_2): %lu !! \n", sizeof(data_2_t));
+    printf("size of struct(data_3): %lu !! \n", sizeof(data_3_t));
+    
+    /* rvalue - right value && lvalue - left value */
+    
+    /* converting string to int value has 2 methods */
+    /* first method !! */
+    char str_1[10] = "1234";
+    int i = atoi(str_1);
+    printf("str_1: %s | int i: %d !! \n", str_1, i);
+    
+    /* second method- scanf method need to check !! */
+    
+    /*
+     * prevent same header file getting included multiple times 
+     * We can use ifndef and define preprocessors. Say the header file is hdrfile.h, then we can write the header file like -
+     *  #ifndef _HDRFILE_H_
+        #define _HDRFILE_H_
+            statement needs to excute !!
+        #endif
+     */
+    
+    
+    return 0;
+}
